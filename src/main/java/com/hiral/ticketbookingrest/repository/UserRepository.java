104bd0f89b4d0152a516658ef89d3443d92c73a9
HiralHirpara <hirparahiral01@gmail.com>
2024-02-07 14:10:20 -0800

    Added User repository and User service

--- src/main/java/com/hiral/ticketbookingrest/repository/UserRepository.java
+++ src/main/java/com/hiral/ticketbookingrest/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.hiral.ticketbookingrest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.hiral.ticketbookingrest.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
 }
--- src/main/java/com/hiral/ticketbookingrest/service/UserService.java
+++ src/main/java/com/hiral/ticketbookingrest/service/UserService.java
@@ -1,56 +1,23 @@
 package com.hiral.ticketbookingrest.service;
 
-import org.springframework.stereotype.Service;
-
 import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.hiral.ticketbookingrest.model.User;
-import com.hiral.ticketbookingrest.repository.UserRepository;
 
-@Service
-public class UserService {
+public interface UserService {
 	
-	@Autowired
-	private UserRepository userRepositoty;
+	// Get all User list
+	List<User> fetchUserList();
 	
-	public List<User> findAll(){
-		return userRepositoty.findAll();
-	}
+	// Get specific User
+	User fetchUser(Long id);
 	
-	public User fetchUser(Long id){
-		Optional<User> user = userRepositoty.findById(id);
-		
-		if(user.isPresent()) {
-			return user.get();
-		}
-		return null;
-	}
-
-	public User addUser(User user) {
-		
-		return userRepositoty.save(user);
-	}
-
-	public User updateUser(Long id, User user) {
-		if(user.getId() != id) {
-			return null;
-		}
-		Optional<User> oldUser =userRepositoty.findById(id);
-		
-		if(oldUser.isPresent()) {
-			return userRepositoty.save(user);
-		}
-		return null;
-	}
-
-	public void deleteUser(Long id) {
-		userRepositoty.deleteById(id);
-	}
+	// Save User
+	User addUser(User user);
 	
+	// Update User
+	User updateUser(Long id, User user);
 	
-	
-
+	// Delete User
+	void deleteUser(Long id);
 }
