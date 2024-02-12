987da77658ac0243c2a2be0c03f45c45353d4a66
HiralHirpara <hirparahiral01@gmail.com>
2024-02-11 21:24:09 -0800

    Made few changes in UserController file

--- src/main/java/com/hiral/ticketbookingrest/controller/UserController.java
+++ src/main/java/com/hiral/ticketbookingrest/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,19 +26,26 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	@GetMapping
-	public List<User> getUsers(){
+	@GetMapping()
+	public ResponseEntity<List<User>> getUsers(){
 		List<User> response = userService.fetchUserList();
-		return response;
+		if(response.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+		}
+		return ResponseEntity.ok(response);
 	}
 	
 	@GetMapping("/{id}")	
-	public User getUser(@PathVariable("id") Long id) {
-		return userService.fetchUser(id);
+	public ResponseEntity<User> getUser(@PathVariable("id") Long id) {
+		User user = userService.fetchUser(id);
+		if(user == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(user);
+		}
+		return ResponseEntity.ok(user);
 		
 	}
 	
-	@PostMapping
+	@PostMapping()
 	@ResponseStatus(HttpStatus.CREATED)
 	public User addUser(@RequestBody User user) {
 		return userService.addUser(user);
