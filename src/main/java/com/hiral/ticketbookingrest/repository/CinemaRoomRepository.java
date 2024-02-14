7328826cd84d6476509218284156740969767b65
HiralHirpara <hirparahiral01@gmail.com>
2024-02-13 21:29:05 -0800

    added CinemaRoom repository class

---
+++ src/main/java/com/hiral/ticketbookingrest/repository/CinemaRoomRepository.java
@@ -0,0 +1,13 @@
+package com.hiral.ticketbookingrest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import org.springframework.stereotype.Repository;
+
+import com.hiral.ticketbookingrest.model.CinemaRoom;
+
+@Repository
+public interface CinemaRoomRepository extends JpaRepository<CinemaRoom,Long>{
+
+}
