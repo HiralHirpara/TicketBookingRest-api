378e08021afe0155511b6d784a189d70bb75f2a7
HiralHirpara <hirparahiral01@gmail.com>
2024-02-15 21:58:34 -0800

    fix Booking service class

--- src/main/java/com/hiral/ticketbookingrest/service/BookingService.java
+++ src/main/java/com/hiral/ticketbookingrest/service/BookingService.java
@@ -3,6 +3,7 @@ package com.hiral.ticketbookingrest.service;
 import java.util.List;
 
 import com.hiral.ticketbookingrest.model.Booking;
+import com.hiral.ticketbookingrest.model.dto.BookingDTO;
 
 public interface BookingService {
 	
@@ -10,7 +11,7 @@ public interface BookingService {
 	
 	Booking fetchBooking(Long id);
 	
-	Booking addBooking(Booking booking);
+	Booking addBooking(BookingDTO bookingDTO);
 	
 	Booking updateBooking(Long id, Booking booking);
 	
